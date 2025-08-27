62ebf29dbe0db648e3e1b9919c011a4047c703d2
nahnah <nandanrmenon@gmail.com>
2025-08-27 21:07:58 +0100

    revert dynamic colors
    issues with color schemes and elevations

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,13 +1,18 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 ThemeData darkTheme({ColorScheme? dynamicScheme}) {
-  final scheme = dynamicScheme ??
-      ColorScheme.fromSeed(
-        seedColor: const Color(0xFFB8DF54),
-        brightness: Brightness.dark,
-      );
+  // final scheme = dynamicScheme ??
+  //     ColorScheme.fromSeed(
+  //       seedColor: const Color(0xFFB8DF54),
+  //       brightness: Brightness.dark,
+  //     );
+  final scheme = ColorScheme.fromSeed(
+    seedColor: const Color(0xFF64D3FF),
+    brightness: Brightness.dark,
+  );
   return ThemeData(
-    colorScheme: scheme,
+    colorScheme: scheme.harmonized(),
     appBarTheme: const AppBarTheme(),
     inputDecorationTheme: inputDecoration(),
     progressIndicatorTheme: const ProgressIndicatorThemeData(
@@ -17,13 +22,17 @@ ThemeData darkTheme({ColorScheme? dynamicScheme}) {
 }
 
 ThemeData lightTheme({ColorScheme? dynamicScheme}) {
-  final scheme = dynamicScheme ??
-      ColorScheme.fromSeed(
-        seedColor: const Color(0xFFB8DF54),
-        brightness: Brightness.light,
-      );
+  // final scheme = dynamicScheme ??
+  //     ColorScheme.fromSeed(
+  //       seedColor: const Color(0xFFB8DF54),
+  //       brightness: Brightness.light,
+  //     );
+  final scheme = ColorScheme.fromSeed(
+    seedColor: const Color(0xFF64D3FF),
+    brightness: Brightness.light,
+  );
   return ThemeData(
-    colorScheme: scheme,
+    colorScheme: scheme.harmonized(),
     appBarTheme: const AppBarTheme(),
     inputDecorationTheme: inputDecoration(),
     progressIndicatorTheme: const ProgressIndicatorThemeData(
