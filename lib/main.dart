a1f4c98a9d2cac7c191eeac1e89a4ec37ca5bbe9
nahnah <nandanrmenon@gmail.com>
2025-08-27 15:18:54 +0100

    Implement dynamic color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart'; // Add this import
 import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:nahcon/models/jellyfin_item.dart';
@@ -18,29 +19,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'nahCon',
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: false,
-      home: FutureBuilder(
-        future: Future.wait([
-          Future.delayed(const Duration(seconds: 2)),
-          JellyfinService().tryAutoLogin(),
-          // Preload movies in background if logged in
-          JellyfinService().getAllMovies().catchError((e) => <JellyfinItem>[]),
-        ]),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const SplashScreen();
-          }
-          if (snapshot.hasData && snapshot.data![1] == true) {
-            return App(service: JellyfinService());
-          }
-          return const LoginScreen();
-        },
-      ),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        return MaterialApp(
+          title: 'nahCon',
+          theme: lightTheme(dynamicScheme: lightDynamic),
+          darkTheme: darkTheme(dynamicScheme: darkDynamic),
+          themeMode: ThemeMode.system,
+          debugShowCheckedModeBanner: false,
+          home: FutureBuilder(
+            future: Future.wait([
+              Future.delayed(const Duration(seconds: 2)),
+              JellyfinService().tryAutoLogin(),
+              // Preload movies in background if logged in
+              JellyfinService()
+                  .getAllMovies()
+                  .catchError((e) => <JellyfinItem>[]),
+            ]),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SplashScreen();
+              }
+              if (snapshot.hasData && snapshot.data![1] == true) {
+                return App(service: JellyfinService());
+              }
+              return const LoginScreen();
+            },
+          ),
+        );
+      },
     );
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,38 +1,39 @@
 import 'package:flutter/material.dart';
 
-ThemeData get darkTheme {
+ThemeData darkTheme({ColorScheme? dynamicScheme}) {
+  final scheme = dynamicScheme ??
+      ColorScheme.fromSeed(
+        seedColor: const Color(0xFFB8DF54),
+        brightness: Brightness.dark,
+      );
   return ThemeData(
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: const Color(0xFFB8DF54),
-      brightness: Brightness.dark,
-      // dynamicSchemeVariant: DynamicSchemeVariant.content,
-    ),
-    appBarTheme: const AppBarTheme(centerTitle: false),
+    colorScheme: scheme,
+    appBarTheme: const AppBarTheme(),
     inputDecorationTheme: inputDecoration(),
-    progressIndicatorTheme: ProgressIndicatorThemeData(
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
       year2023: false,
     ),
   );
 }
 
-ThemeData get lightTheme {
+ThemeData lightTheme({ColorScheme? dynamicScheme}) {
+  final scheme = dynamicScheme ??
+      ColorScheme.fromSeed(
+        seedColor: const Color(0xFFB8DF54),
+        brightness: Brightness.light,
+      );
   return ThemeData(
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: const Color(0xFFB8DF54),
-      // dynamicSchemeVariant: DynamicSchemeVariant.content,
-    ),
-    appBarTheme: const AppBarTheme(centerTitle: false),
+    colorScheme: scheme,
+    appBarTheme: const AppBarTheme(),
     inputDecorationTheme: inputDecoration(),
-    progressIndicatorTheme: ProgressIndicatorThemeData(
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
       year2023: false,
     ),
   );
 }
 
-InputDecorationThemeData inputDecoration() {
-  return InputDecorationThemeData(
-    // filled: true,
-    // alignLabelWithHint: true,
+InputDecorationTheme inputDecoration() {
+  return const InputDecorationTheme(
     border: OutlineInputBorder(),
   );
 }
