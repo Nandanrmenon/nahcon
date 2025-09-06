d3755f55e0268b7f7ce13b3f5224d0cbf71386e8
nahnah <nandanrmenon@gmail.com>
2025-09-07 00:31:27 +0100

    [bug]: fix body going behind statusbar

--- lib/screens/series_screen.dart
+++ lib/screens/series_screen.dart
@@ -14,68 +14,70 @@ class SeriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return FutureBuilder<List<JellyfinItem>>(
-            future: service.getAllSeries(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              }
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return FutureBuilder<List<JellyfinItem>>(
+              future: service.getAllSeries(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
 
-              if (!snapshot.hasData) {
-                return const Center(child: CircularProgressIndicator());
-              }
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
 
-              return GridView.builder(
-                padding: const EdgeInsets.all(ResponsiveGrid.gridSpacing),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: ResponsiveGrid.columnCount(constraints),
-                  childAspectRatio: ResponsiveGrid.posterAspectRatio,
-                  crossAxisSpacing: ResponsiveGrid.gridSpacing,
-                  mainAxisSpacing: ResponsiveGrid.gridSpacing,
-                ),
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  final isDesktop = ResponsiveGrid.isDesktop(constraints);
-                  final series = snapshot.data![index];
-                  return MovieCard(
-                    title: series.name,
-                    posterUrl: series.imageUrl != null
-                        ? service.getImageUrl(series.imageUrl)
-                        : null,
-                    onTap: () {
-                      if (isDesktop) {
-                        showModalBottomSheet(
-                          context: context,
-                          scrollControlDisabledMaxHeightRatio: 0.9,
-                          constraints:
-                              BoxConstraints(minWidth: 300, maxWidth: 1200),
-                          clipBehavior: Clip.antiAlias,
-                          builder: (context) => Center(
-                            child: SeriesDetailsScreen(
-                              series: series,
-                              service: service,
+                return GridView.builder(
+                  padding: const EdgeInsets.all(ResponsiveGrid.gridSpacing),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: ResponsiveGrid.columnCount(constraints),
+                    childAspectRatio: ResponsiveGrid.posterAspectRatio,
+                    crossAxisSpacing: ResponsiveGrid.gridSpacing,
+                    mainAxisSpacing: ResponsiveGrid.gridSpacing,
+                  ),
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    final isDesktop = ResponsiveGrid.isDesktop(constraints);
+                    final series = snapshot.data![index];
+                    return MovieCard(
+                      title: series.name,
+                      posterUrl: series.imageUrl != null
+                          ? service.getImageUrl(series.imageUrl)
+                          : null,
+                      onTap: () {
+                        if (isDesktop) {
+                          showModalBottomSheet(
+                            context: context,
+                            scrollControlDisabledMaxHeightRatio: 0.9,
+                            constraints:
+                                BoxConstraints(minWidth: 300, maxWidth: 1200),
+                            clipBehavior: Clip.antiAlias,
+                            builder: (context) => Center(
+                              child: SeriesDetailsScreen(
+                                series: series,
+                                service: service,
+                              ),
                             ),
-                          ),
-                        );
-                      } else {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => SeriesDetailsScreen(
-                              series: series,
-                              service: service,
+                          );
+                        } else {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => SeriesDetailsScreen(
+                                series: series,
+                                service: service,
+                              ),
                             ),
-                          ),
-                        );
-                      }
-                    },
-                  );
-                },
-              );
-            },
-          );
-        },
+                          );
+                        }
+                      },
+                    );
+                  },
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
