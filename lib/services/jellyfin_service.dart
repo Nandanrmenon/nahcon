55ac2f7aca945bbba8d55adf1debe6b60c665e1a
nahnah <nandanrmenon@gmail.com>
2025-09-11 22:32:00 +0100

    [improv]: update client version in JellyfinService

--- lib/services/jellyfin_service.dart
+++ lib/services/jellyfin_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:nahcon/utils/constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/jellyfin_item.dart';
@@ -17,10 +18,10 @@ class JellyfinService {
   String? userId;
   String? username;
   String? serverName;
-  static const String _clientName = 'nahCon';
+  static const String _clientName = kAppName;
   static const String _deviceId = 'flutter_app_1';
   static const String _deviceName = 'Flutter Mobile';
-  static const String _version = '1.0.1';
+  static const String _version = kAppVersion;
   static const String _apiVersion = '10.11.0';
 
   static const String _profilesKey = 'user_profiles';
